57c3ea80b40dd85b4c2b621729c722832e7effe5
Johann Fuechsl <johann@fuechsl.co>
2018-10-24 11:31:13 +0200

    Put master.key on efs

--- deploy/before_compile_assets.rb
+++ deploy/before_compile_assets.rb
@@ -1,3 +1,3 @@
 on_app_servers_and_utilities do
-  run! "ln -snf #{config.shared_path}/config/master.key #{config.release_path}/config/master.key"
+  run! "ln -snf /efs/secrets/master.key #{config.release_path}/config/master.key"
 end
